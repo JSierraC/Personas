1c11d7f11969ca3159b15eca17a79c6f87a4730d
Juan Jose Sierra <jota853@hotmail.com>
2019-07-13 10:23:38 -0500

    tabla terminada

--- app/src/main/java/com/example/personas/CrearPersonas.java
+++ app/src/main/java/com/example/personas/CrearPersonas.java
@@ -1,13 +1,53 @@
 package com.example.personas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CrearPersonas extends AppCompatActivity {
-
+    private EditText cedula, nombre, apellido;
+    private Button btAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_personas);
+        
+        cedula  = findViewById(R.id.txtCedula);
+        nombre  = findViewById(R.id.txtNombre);
+        apellido  = findViewById(R.id.txtApellido);
+        btAdd = findViewById(R.id.btnAgregar);
+        final Datos dato = new Datos();
+/*
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Persona per = new Persona(cedula.getText().toString(), nombre.getText().toString(), apellido.getText().toString());
+                Datos.guardar(per);
+            }
+        });
+ */
+    }
+
+    public void guardar(View v){
+        String nom, apell, ced;
+
+        ced   = cedula.getText().toString();
+        nom   = nombre.getText().toString();
+        apell = apellido.getText().toString();
+
+        Persona p = new Persona(ced, nom, apell);
+        p.guardar();
+        limpiar(v);
+
+    }
+
+    public void limpiar(View v){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
     }
 }
--- app/src/main/java/com/example/personas/ListarPersonas.java
+++ app/src/main/java/com/example/personas/ListarPersonas.java
@@ -1,13 +1,57 @@
 package com.example.personas;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListarPersonas extends AppCompatActivity {
-
+    private TableLayout tabla;
+    private ArrayList<Persona> personas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_personas);
+
+        tabla = findViewById(R.id.tabla);
+        personas = Datos.obtener();
+
+        for (int i=0;i<personas.size();i++) {
+
+            TableRow fila = new TableRow(this);
+            TextView c1 = new TextView(this);
+            TextView c2 = new TextView(this);
+            TextView c3 = new TextView(this);
+            TextView c4 = new TextView(this);
+
+            c1.setText("" + (i + 1));
+            c1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            c1.setTextColor(Color.YELLOW);
+
+            c2.setText(personas.get(i).getCedula());
+            c2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            c2.setTextColor(Color.RED);
+
+            c3.setText(personas.get(i).getNombre());
+            c3.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            c3.setTextColor(Color.GREEN);
+
+            c4.setText(personas.get(i).getApellido());
+            c4.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            c4.setTextColor(Color.BLUE);
+
+            fila.addView(c1);
+            fila.addView(c2);
+            fila.addView(c3);
+            fila.addView(c4);
+
+            tabla.addView(fila);
+        }
+
     }
 }
